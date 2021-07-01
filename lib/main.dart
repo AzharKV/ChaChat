516415ed59706ed8367534173d42e230bac64315
Azhar <mohammedazharkv@gmail.com>
2021-07-01 13:59:10 +0530

    update firebase project verification

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'Screens/ChatScreen.dart';
 import 'Screens/LoginScreen.dart';
 import 'Screens/RegistrationScreen.dart';
 import 'Screens/WelcomeScreen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
